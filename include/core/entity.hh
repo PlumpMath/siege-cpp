3a73425a75d8852d9a3857766a0c9b0256147db9
Tim Čas <darkuranium@gmail.com>
2012-09-26 13:55:56 +0200

    Fixed an untracked file problem... hopefully

---
+++ include/core/entity.hh
@@ -0,0 +1,273 @@
+/*
+ * Copyright (c) 2007 SIEGE Development Team
+ * All rights reserved.
+ *
+ * This file is part of libSIEGE.
+ *
+ * This software is copyrighted work licensed under the terms of the
+ * 2-clause BSD license. Please consult the file "COPYING.txt" for
+ * details.
+ *
+ * If you did not recieve the file with this program, please email
+ * Tim Chas <darkuranium@gmail.com>.
+ */
+
+#ifndef __CPP_SIEGE_CORE_ENTITY_H__
+#define __CPP_SIEGE_CORE_ENTITY_H__
+
+#include "../common.hh"
+#include "../graphics/sprite.hh"
+
+#define ENT(cent) ((Entity*)(cent)->data)
+#define BIND(name) handle->name = c_##name;
+
+namespace siege
+{
+    namespace core
+    {
+        class Entity: public SGClass<c::SGEntity>
+        {
+        private:
+            static c::SGbool c_cbInside(c::SGEntity* entity, float x, float y) { return ENT(entity)->cbInside(x, y); }
+
+            static void c_lcDestroy(c::SGEntity* entity) { ENT(entity)->lcDestroy(); }
+
+            static void c_evInit(c::SGEntity* entity) { ENT(entity)->evInit(); }
+            static void c_evDeinit(c::SGEntity* entity) { ENT(entity)->evDeinit(); }
+
+            static void c_evStart(c::SGEntity* entity) { ENT(entity)->evStart(); }
+            static void c_evExit(c::SGEntity* entity) { ENT(entity)->evExit(); }
+
+            static void c_evTick(c::SGEntity* entity) { ENT(entity)->evTick(); }
+            static void c_evTickBegin(c::SGEntity* entity) { ENT(entity)->evTickBegin(); }
+            static void c_evTickEnd(c::SGEntity* entity) { ENT(entity)->evTickEnd(); }
+            static void c_evDraw(c::SGEntity* entity) { ENT(entity)->evDraw(); }
+
+            static void c_evWindowOpen(c::SGEntity* entity) { ENT(entity)->evWindowOpen(); }
+            static void c_evWindowClose(c::SGEntity* entity) { ENT(entity)->evWindowClose(); }
+            static void c_evWindowResize(c::SGEntity* entity, c::SGuint width, c::SGuint height) { ENT(entity)->evWindowResize(width, height); }
+
+            static void c_evMouseButton(c::SGEntity* entity, c::SGuint button) { ENT(entity)->evMouseButton(button); }
+            static void c_evMouseButtonPress(c::SGEntity* entity, c::SGuint button) { ENT(entity)->evMouseButtonPress(button); }
+            static void c_evMouseButtonRelease(c::SGEntity* entity, c::SGuint button) { ENT(entity)->evMouseButtonRelease(button); }
+
+            static void c_evMouseButtonLeft(c::SGEntity* entity) { ENT(entity)->evMouseButtonLeft(); }
+            static void c_evMouseButtonLeftPress(c::SGEntity* entity) { ENT(entity)->evMouseButtonLeftPress(); }
+            static void c_evMouseButtonLeftRelease(c::SGEntity* entity) { ENT(entity)->evMouseButtonLeftRelease(); }
+
+            static void c_evMouseButtonRight(c::SGEntity* entity) { ENT(entity)->evMouseButtonRight(); }
+            static void c_evMouseButtonRightPress(c::SGEntity* entity) { ENT(entity)->evMouseButtonRightPress(); }
+            static void c_evMouseButtonRightRelease(c::SGEntity* entity) { ENT(entity)->evMouseButtonRightRelease(); }
+
+            static void c_evMouseButtonMiddle(c::SGEntity* entity) { ENT(entity)->evMouseButtonMiddle(); }
+            static void c_evMouseButtonMiddlePress(c::SGEntity* entity) { ENT(entity)->evMouseButtonMiddlePress(); }
+            static void c_evMouseButtonMiddleRelease(c::SGEntity* entity) { ENT(entity)->evMouseButtonMiddleRelease(); }
+
+            static void c_evMouseMove(c::SGEntity* entity, c::SGint x, c::SGint y) { ENT(entity)->evMouseMove(x, y); }
+            static void c_evMouseWheel(c::SGEntity* entity, c::SGint wheel) { ENT(entity)->evMouseWheel(wheel); }
+
+            static void c_evKeyboardKey(c::SGEntity* entity, c::SGuint key) { ENT(entity)->evKeyboardKey(key); }
+            static void c_evKeyboardKeyPress(c::SGEntity* entity, c::SGuint key) { ENT(entity)->evKeyboardKeyPress(key); }
+            static void c_evKeyboardKeyRelease(c::SGEntity* entity, c::SGuint key) { ENT(entity)->evKeyboardKeyRelease(key); }
+            static void c_evKeyboardKeyRepeat(c::SGEntity* entity, c::SGuint key) { ENT(entity)->evKeyboardKeyRepeat(key); }
+
+            static void c_evKeyboardCharPress(c::SGEntity* entity, c::SGdchar chr) { ENT(entity)->evKeyboardCharPress(chr); }
+
+            static void c_evJoystickButton(c::SGEntity* entity, c::SGuint joy, c::SGuint button) { ENT(entity)->evJoystickButton(joy, button); }
+            static void c_evJoystickButtonPress(c::SGEntity* entity, c::SGuint joy, c::SGuint button) { ENT(entity)->evJoystickButtonPress(joy, button); }
+            static void c_evJoystickButtonRelease(c::SGEntity* entity, c::SGuint joy, c::SGuint button) { ENT(entity)->evJoystickButtonRelease(joy, button); }
+            static void c_evJoystickMove(c::SGEntity* entity, c::SGuint joy, float* axis, size_t numaxis) { ENT(entity)->evJoystickMove(joy, axis, numaxis); }
+
+            // networking goes here
+
+            static void c_evLevelStart(c::SGEntity* entity) { ENT(entity)->evLevelStart(); }
+            static void c_evLevelEnd(c::SGEntity* entity) { ENT(entity)->evLevelEnd(); }
+
+            static void c_evUser(c::SGEntity* entity, c::SGuint id, va_list args) { ENT(entity)->evUser(id, args); }
+
+            graphics::PSprite sprite;
+            void create()
+            {
+                handle = c::sgEntityCreate();
+                handle->data = this;
+
+                BIND(cbInside);
+
+                BIND(lcDestroy);
+
+                BIND(evInit);
+                BIND(evDeinit);
+
+                BIND(evStart);
+                BIND(evExit);
+
+                BIND(evTick);
+                BIND(evTickBegin);
+                BIND(evTickEnd);
+                BIND(evDraw);
+
+                BIND(evWindowOpen);
+                BIND(evWindowClose);
+                BIND(evWindowResize);
+
+                BIND(evMouseButton);
+                BIND(evMouseButtonPress);
+                BIND(evMouseButtonRelease);
+
+                BIND(evMouseButtonLeft);
+                BIND(evMouseButtonLeftPress);
+                BIND(evMouseButtonLeftRelease);
+
+                BIND(evMouseButtonRight);
+                BIND(evMouseButtonRightPress);
+                BIND(evMouseButtonRightRelease);
+
+                BIND(evMouseButtonMiddle);
+                BIND(evMouseButtonMiddlePress);
+                BIND(evMouseButtonMiddleRelease);
+
+                BIND(evMouseMove);
+                BIND(evMouseWheel);
+
+                BIND(evKeyboardKey);
+                BIND(evKeyboardKeyPress);
+                BIND(evKeyboardKeyRelease);
+                BIND(evKeyboardKeyRepeat);
+
+                BIND(evKeyboardCharPress);
+
+                BIND(evJoystickButton);
+                BIND(evJoystickButtonPress);
+                BIND(evJoystickButtonRelease);
+                BIND(evJoystickMove);
+
+                // networking goes here
+
+                BIND(evLevelStart);
+                BIND(evLevelEnd);
+
+                BIND(evUser);
+            }
+        public:
+            static void destroyAll() { c::sgEntityDestroyAll(); }
+            /*SGList* SG_EXPORT sgEntityFind(const char* name);
+
+            void SG_EXPORT sgEntityEventSignalv(size_t num, va_list args);
+            void SG_EXPORT sgEntityEventSignal(size_t num, ...);
+            void SG_EXPORT sgEntityEventStop(void);*/
+
+            Entity(): sprite() { create(); }
+            virtual ~Entity() { if(handle) c::sgEntityDestroy(handle); }
+
+            virtual void destroy() { c::sgEntityDestroy(handle); handle = NULL; }
+
+            void setName(const char* name) { c::sgEntitySetName(handle, name); }
+            char* getName() { return c::sgEntityGetName(handle); }
+
+            void setSprite(graphics::PSprite sprite) { c::sgEntitySetSprite(handle, sprite->getHandle()); this->sprite = sprite; }
+            graphics::PSprite getSprite() { return this->sprite; }
+
+            //void SG_EXPORT sgEntitySetMask(SGEntity* entity, struct SGMask* mask);
+            //struct SGMask* SG_EXPORT sgEntityGetMask(SGEntity* entity);
+
+            //void SG_EXPORT sgEntitySetPhysicsBody(SGEntity* entity, struct SGPhysicsBody* body);
+            //struct SGPhysicsBody* SG_EXPORT sgEntityGetPhysicsBody(SGEntity* entity);
+
+            //void SG_EXPORT sgEntitySetAudioSource(SGEntity* entity, struct SGAudioSource* source);
+            //struct SGAudioSource* SG_EXPORT sgEntityGetAudioSource(SGEntity* entity);
+
+            void setPos(float x, float y) { c::sgEntitySetPos(handle, x, y); }
+            void setPosX(float x) { c::sgEntitySetPosX(handle, x); }
+            void setPosY(float y) { c::sgEntitySetPosY(handle, y); }
+            void getPos(float* x, float* y) { c::sgEntityGetPos(handle, x, y); }
+            void getPos(float& x, float& y) { c::sgEntityGetPos(handle, &x, &y); }
+            float getPosX() { return c::sgEntityGetPosX(handle); }
+            float getPosY() { return c::sgEntityGetPosY(handle); }
+
+            void setDepth(float depth) { c::sgEntitySetDepth(handle, depth); }
+            float getDepth() { return c::sgEntityGetDepth(handle); }
+
+            void setAngleRads(float rads) { c::sgEntitySetAngleRads(handle, rads); }
+            float getAngleRads() { return c::sgEntityGetAngleRads(handle); }
+
+            void setAngleDegs(float degs) { c::sgEntitySetAngleDegs(handle, degs); }
+            float getAngleDegs() { return c::sgEntityGetAngleDegs(handle); }
+
+            virtual void draw() { c::sgEntityDraw(handle); }
+
+            /*void SG_EXPORT (*lcCollision)(struct SGEntity* entity, struct SGEntity* other, struct SGPhysicsCollision* coll);
+            void SG_EXPORT (*lcCollisionOne)(struct SGEntity* entity, struct SGEntity* other, struct SGPhysicsCollision* coll);
+            void SG_EXPORT (*lcCollisionTwo)(struct SGEntity* entity, struct SGEntity* other, struct SGPhysicsCollision* coll);
+
+            void SG_EXPORT (*lcCollisionBegin)(struct SGEntity* entity, struct SGEntity* other, struct SGPhysicsCollision* coll);
+            void SG_EXPORT (*lcCollisionOneBegin)(struct SGEntity* entity, struct SGEntity* other, struct SGPhysicsCollision* coll);
+            void SG_EXPORT (*lcCollisionTwoBegin)(struct SGEntity* entity, struct SGEntity* other, struct SGPhysicsCollision* coll);
+
+            void SG_EXPORT (*lcCollisionEnd)(struct SGEntity* entity, struct SGEntity* other, struct SGPhysicsCollision* coll);
+            void SG_EXPORT (*lcCollisionOneEnd)(struct SGEntity* entity, struct SGEntity* other, struct SGPhysicsCollision* coll);
+            void SG_EXPORT (*lcCollisionTwoEnd)(struct SGEntity* entity, struct SGEntity* other, struct SGPhysicsCollision* coll);*/
+
+            virtual bool cbInside(float x, float y) { return false; }
+
+            virtual void lcDestroy() {}
+
+            virtual void evInit() {}
+            virtual void evDeinit() {}
+
+            virtual void evStart() {}
+            virtual void evExit() {}
+
+            virtual void evTick() {}
+            virtual void evTickBegin() {}
+            virtual void evTickEnd() {}
+            virtual void evDraw() { if(handle->visible) draw(); }
+
+            virtual void evWindowOpen() {}
+            virtual void evWindowClose() {}
+            virtual void evWindowResize(c::SGuint width, c::SGuint height) {}
+
+            virtual void evMouseButton(c::SGuint button) {}
+            virtual void evMouseButtonPress(c::SGuint button) {}
+            virtual void evMouseButtonRelease(c::SGuint button) {}
+
+            virtual void evMouseButtonLeft() {}
+            virtual void evMouseButtonLeftPress() {}
+            virtual void evMouseButtonLeftRelease() {}
+
+            virtual void evMouseButtonRight() {}
+            virtual void evMouseButtonRightPress() {}
+            virtual void evMouseButtonRightRelease() {}
+
+            virtual void evMouseButtonMiddle() {}
+            virtual void evMouseButtonMiddlePress() {}
+            virtual void evMouseButtonMiddleRelease() {}
+
+            virtual void evMouseMove(c::SGint x, c::SGint y) {}
+            virtual void evMouseWheel(c::SGint wheel) {}
+
+            virtual void evKeyboardKey(c::SGuint key) {}
+            virtual void evKeyboardKeyPress(c::SGuint key) {}
+            virtual void evKeyboardKeyRelease(c::SGuint key) {}
+            virtual void evKeyboardKeyRepeat(c::SGuint key) {}
+
+            virtual void evKeyboardCharPress(c::SGdchar chr) {}
+
+            virtual void evJoystickButton(c::SGuint joy, c::SGuint button) {}
+            virtual void evJoystickButtonPress(c::SGuint joy, c::SGuint button) {}
+            virtual void evJoystickButtonRelease(c::SGuint joy, c::SGuint button) {}
+            virtual void evJoystickMove(c::SGuint joy, float* axis, size_t numaxis) {}
+
+            // networking goes here
+
+            virtual void evLevelStart() {}
+            virtual void evLevelEnd() {}
+
+            virtual void evUser(c::SGuint id, va_list args) {}
+        } ;
+        RTYPE(Entity);
+    }
+}
+
+#undef BIND
+
+#endif // __CPP_SIEGE_CORE_ENTITY_H__
---
+++ include/graphics/sprite.hh
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2007 SIEGE Development Team
+ * All rights reserved.
+ *
+ * This file is part of libSIEGE.
+ *
+ * This software is copyrighted work licensed under the terms of the
+ * 2-clause BSD license. Please consult the file "COPYING.txt" for
+ * details.
+ *
+ * If you did not recieve the file with this program, please email
+ * Tim Chas <darkuranium@gmail.com>.
+ */
+
+#ifndef __CPP_SIEGE_GRAPHICS_SPRITE_H__
+#define __CPP_SIEGE_GRAPHICS_SPRITE_H__
+
+#include "../common.hh"
+
+namespace siege
+{
+    namespace graphics
+    {
+        class Sprite: public SGClass<c::SGSprite>
+        {
+        private:
+            //void create(Texture* tex, float xoffset, float yoffset) {}
+            //void create(Texture* tex) {}
+            void create(const char* fname, float xoffset, float yoffset) { handle = c::sgSpriteCreateFile2f(fname, xoffset, yoffset); }
+            void create(const char* fname) { handle = c::sgSpriteCreateFile(fname); }
+
+        public:
+            /// \todo overload
+            //Sprite(Texture* tex, float xoffset, float yoffset)
+            //Sprite(Texture* tex)
+            Sprite(const char* fname, float xoffset, float yoffset) { create(fname, xoffset, yoffset); }
+            Sprite(const char* fname) { create(fname); }
+            ~Sprite() { if(handle) c::sgSpriteDestroy(handle); }
+
+            void destroy() { c::sgSpriteDestroy(handle); handle = NULL; }
+
+            bool addFrameFile(const char* fname) { return c::sgSpriteAddFrameFile(handle, fname); }
+
+            void setImage(float image) { c::sgSpriteSetImage(handle, image); }
+            float getImage() { return c::sgSpriteGetImage(handle); }
+
+            void setSpeed(float speed) { c::sgSpriteSetSpeed(handle, speed); }
+            float getSpeed() { return c::sgSpriteGetSpeed(handle); }
+
+            void drawRads(float x, float y, float z, float xscale, float yscale, float angle) { c::sgSpriteDrawRads3f2f1f(handle, x, y, z, xscale, yscale, angle); }
+            void drawDegs(float x, float y, float z, float xscale, float yscale, float angle) { c::sgSpriteDrawDegs3f2f1f(handle, x, y, z, xscale, yscale, angle); }
+            void drawRads(float x, float y, float xscale, float yscale, float angle) { c::sgSpriteDrawRads2f2f1f(handle, x, y, xscale, yscale, angle); }
+            void drawDegs(float x, float y, float xscale, float yscale, float angle) { c::sgSpriteDrawDegs2f2f1f(handle, x, y, xscale, yscale, angle); }
+            void drawRads(float x, float y, float z, float angle) { c::sgSpriteDrawRads3f1f(handle, x, y, z, angle); }
+            void drawDegs(float x, float y, float z, float angle) { c::sgSpriteDrawDegs3f1f(handle, x, y, z, angle); }
+            void drawRads(float x, float y, float angle) { c::sgSpriteDrawRads2f1f(handle, x, y, angle); }
+            void drawDegs(float x, float y, float angle) { c::sgSpriteDrawDegs2f1f(handle, x, y, angle); }
+            void draw(float x, float y, float z, float xscale, float yscale) { c::sgSpriteDraw3f2f(handle, x, y, z, xscale, yscale); }
+            void draw(float x, float y, float xscale, float yscale) { c::sgSpriteDraw2f2f(handle, x, y, xscale, yscale); }
+            void draw(float x, float y, float z) { c::sgSpriteDraw3f(handle, x, y, z); }
+            void draw(float x, float y) { c::sgSpriteDraw2f(handle, x, y); }
+
+            void getSize(c::SGuint* width, c::SGuint* height) { c::sgSpriteGetSize(handle, width, height); }
+            void getSize(c::SGuint& width, c::SGuint& height) { c::sgSpriteGetSize(handle, &width, &height); }
+            c::SGuint getWidth() { return c::sgSpriteGetWidth(handle); }
+            c::SGuint getHeight() { return c::sgSpriteGetHeight(handle); }
+        } ;
+        RTYPE(Sprite);
+    }
+}
+
+#endif // __CPP_SIEGE_GRAPHICS_SPRITE_H__
